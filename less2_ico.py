9e9d06d264c92035bb227c8921d4ba3d5c53fcdd
Dmitry <39635520+Dmitry1973@users.noreply.github.com>
2019-07-18 19:19:43 +0500

    Update less2_ico.py

--- less2_ico.py
+++ less2_ico.py
@@ -4,7 +4,7 @@ site_adr = 'https://icobench.com/icos'
 for N in range (1, 470):
     params = {'page': N}
     site = requests.get(site_adr,params=params)
-    print(site.url)
+    #print(site.url)    #а потом ежик подумал, а зачем печатать ссылки:)
     with open(f'icobench_ico_page_{N}.html', 'w', encoding='utf-8') as d:
         d.write(site.text)
    
