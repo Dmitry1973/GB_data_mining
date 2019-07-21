cf48b585d1a02a6da4f3355b131551d5cb90e8df
Dmitry <39635520+Dmitry1973@users.noreply.github.com>
2019-07-21 22:54:31 +0500

    Update less2_ico.py

--- less2_ico.py
+++ less2_ico.py
@@ -3,16 +3,16 @@ import requests
 import os
 
 
-my_path = 'ico/'
-if not os.path.isdir(my_path):
-    os.mkdir(my_path)
+my_path_ico = 'ico_dir/'
+if not os.path.isdir(my_path_ico):
+    os.mkdir(my_path_ico)
 
 site_adr = 'https://icobench.com/icos'
 for N in range (1, 470):
     params = {'page': N}
     site = requests.get(site_adr,params=params)
     #print(site.url)    #а потом ежик подумал, а зачем печатать ссылки:)
-    with open(f'ico/icobench_ico_page_{N}.html', 'w', encoding='utf-8') as d:
+    with open(f'ico_dir/icobench_ico_page_{N}.html', 'w', encoding='utf-8') as d:
         d.write(site.text)
    
 
