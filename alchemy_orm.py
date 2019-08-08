e29a11916cd91d1eaffac3ac3fa57a17a62dfa56
Dmitry <39635520+Dmitry1973@users.noreply.github.com>
2019-08-08 11:35:00 +0200

    Update alchemy_orm.py

--- alchemy_orm.py
+++ alchemy_orm.py
@@ -1,4 +1,4 @@
-@@ -1,13 +1,13 @@
+#@@ -1,13 +1,13 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, Integer, Float, Boolean
 Base = declarative_base()
