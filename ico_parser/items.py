7fb36046ae162ca18e3bd282f2a371e65ef71790
Dmitry <39635520+Dmitry1973@users.noreply.github.com>
2019-08-08 11:37:43 +0200

    Update items.py
    pasted

--- ico_parser/items.py
+++ ico_parser/items.py
@@ -3,7 +3,7 @@
 # Define here the models for your scraped items
 #
 # See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
+# https://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
 
@@ -13,3 +13,18 @@ class IcoParserItem(scrapy.Item):
     name = scrapy.Field()
     slogan = scrapy.Field()
     description = scrapy.Field()
+    categories = scrapy.Field()
+    official_link = scrapy.Field()
+    project_social_links = scrapy.Field()
+    # profile_rating = scrapy.Field()
+    experts_ratings = scrapy.Field()
+    about = scrapy.Field()
+    team = scrapy.Field()
+    advisors = scrapy.Field()
+
+
+class PersonItem(scrapy.Item):
+    _id = scrapy.Field()
+    person_url = scrapy.Field()
+    name = scrapy.Field()
+    social_links = scrapy.Field()
